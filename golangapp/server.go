ab3172ac0c7eab15c70db364509e46171483cd79
Robert Oros <robertstefanoros@gmail.com>
2022-04-28 10:11:15 +0300

    Modify server

--- golangapp/server.go
+++ golangapp/server.go
@@ -6,45 +6,78 @@ import (
 	"log"
 	"net/http"
 
-	// "html/template"
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func printRows(db *sql.DB) {
-	var id, nume, prenume string
+type Service struct {
+	DB *sql.DB
+}
 
-	row, _ := db.Query("SELECT id, nume, prenume FROM demisol")
-	for row.Next() {
-		row.Scan(&id, &nume, &prenume)
-		log.Println("Person: ", id, " ", nume, " ", prenume)
+func newService(DB *sql.DB) *Service {
+	return &Service{DB: DB}
+}
+
+func connDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "tst:root@tcp(db:3306)/db")
+	if err != nil {
+		return db, fmt.Errorf("open mysql: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return db, fmt.Errorf("ping: %w", err)
+	}
+
+	return db, nil
+}
+
+func main() {
+	db, err := connDB()
+	if err != nil {
+		fmt.Println("connDB %s", err)
+	}
+
+	s := newService(db)
+
+	http.HandleFunc("/delete", s.deleteHandler)
+	http.HandleFunc("/add", s.addHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func addHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "ADD")
-
+func (s *Service) addHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		nume := r.URL.Query().Get("nume")
 		prenume := r.URL.Query().Get("prenume")
-		if err := addData(nume, prenume); err != nil {
-			// err = fmt.Errorf("add data: %w", err)
+		if err := addData(nume, prenume, s.DB); err != nil {
 			log.Printf("%s", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
 }
 
-func addData(nume string, prenume string) error {
-    db, err := sql.Open("mysql", "tst:root@tcp(db:3306)/db")
-	if err != nil {
-		return fmt.Errorf("open mysql: %w", err)
-	}
+func (s *Service) deleteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodDelete {
+		id := r.URL.Query().Get("id")
+		
+		statement, err := s.DB.Prepare("DELETE FROM demisol WHERE ID = ?")
+		if err != nil {
+			fmt.Println("delete from %s", err)
+		}
 
-	if err = db.Ping(); err != nil {
-		return fmt.Errorf("ping: %w", err)
-	}
+		statement.Exec(id)
 
-	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS demisol (id INTEGER PRIMARY KEY, nume TEXT, prenume TEXT)")
+		err = printRows(s.DB)
+		if err != nil {
+			fmt.Println("print rows %s", err)
+		}
+	}
+}
+
+func addData(nume, prenume string, DB *sql.DB) error {
+	statement, err := DB.Prepare("CREATE TABLE IF NOT EXISTS demisol (id INTEGER PRIMARY KEY, nume TEXT, prenume TEXT)")
 	if err != nil {
 		return fmt.Errorf("db prepare create: %w", err)
 	}
@@ -52,7 +85,7 @@ func addData(nume string, prenume string) error {
 		return fmt.Errorf("db exec: %w", err)
 	}
 
-	statement, err = db.Prepare("INSERT INTO demisol (nume, prenume) VALUES (?, ?)")
+	statement, err = DB.Prepare("INSERT INTO demisol (nume, prenume) VALUES (?, ?)")
 	if err != nil {
 		return fmt.Errorf("db prepare insert: %w", err)
 	}
@@ -60,30 +93,27 @@ func addData(nume string, prenume string) error {
 	if _, err = statement.Exec(nume, prenume); err != nil {
 		return fmt.Errorf("db exec: %w", err)
 	}
-	printRows(db)
+
+	err = printRows(DB)
+	if err != nil {
+		fmt.Println("print rows %s", err)
+	}
+
 	return nil
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "DELETE")
+func printRows(db *sql.DB) error {
+	var id, nume, prenume string
 
-	if r.Method == http.MethodDelete {
-		id := r.URL.Query().Get("id")
-		db, _ := sql.Open("mysql", "tst:root@tcp(db:3306)/db")
-		statement, _ := db.Prepare("DELETE FROM demisol WHERE ID = ?")
-		statement.Exec(id)
-
-		printRows(db)
-	}
-}
-
-func main() {
-	http.HandleFunc("/delete", deleteHandler)
-	http.HandleFunc("/add", addHandler)
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	row, err := db.Query("SELECT id, nume, prenume FROM demisol")
+	if err != nil {
+		return fmt.Errorf("select frm demisol: %w", err)
 	}
 
+	for row.Next() {
+		row.Scan(&id, &nume, &prenume)
+		log.Println("Person: ", id, " ", nume, " ", prenume)
+	}
+
+	return nil
 }
